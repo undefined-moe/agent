7bf5204956f3fa4ac8cdc4a596b2df063d8acfa8
undefined <i@undefined.moe>
2023-02-27 11:55:19 +0800

    goodbye, lerna

--- packages/uhk-web/webpack.config.js
+++ packages/uhk-web/webpack.config.js
@@ -1,6 +1,16 @@
+const path = require("path");
+
+const locate = (name) => path.dirname(require.resolve(`${name}/package.json`))
+
 module.exports = {
     "target": "electron-renderer",
     "node": {
         "global": true,
+    },
+    "resolve": {
+        "alias": {
+            "monaco-editor": locate("monaco-editor"),
+            "nouislider": locate("nouislider"),
+        },
     }
 };
